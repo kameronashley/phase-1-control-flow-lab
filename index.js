138a99ce653289186db0362be6c8fc0d8514d967
Kameron Ashley <kameronashley@gmail.com>
2021-09-20 20:22:51 -0700

    commit tree df5413b8a874dc15e3f30a38e6434325ddab7438

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+   if (ride <= 400) {
+    return 'This one is on me!';
+   } else if (ride <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+   } else {
+     return 'No can do.'
+   }
+    
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity (destination) {
+  if (destination = 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return ('No go.')
+  }
 }
